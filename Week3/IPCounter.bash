fe57c66927bbc31f2112e5f7298d380d8d6feb30
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-09-19 21:05:41 -0400

    completing IP Counting assigment

--- Week3/IPCounter.bash
+++ Week3/IPCounter.bash
@@ -6,26 +6,39 @@ read prefix
 #prefix=$(bash prefixLength.bash)
 ipList=""
 
-prefixLength=$( "$prefix" | cut -d "/" -f2 )
+D2B=({0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1})
+
 ipPart1=$(echo "$ip" | cut -d "." -f1)
 ipPart2=$(echo "$ip" | cut -d "." -f2)
 ipPart3=$(echo "$ip" | cut -d "." -f3)
 
-ipPart1Binary=$(echo "obase=2;$ipPart1" | bc)
-ipPart2Binary=$(echo "obase=2;$ipPart2" | bc)
-ipPart3Binary=$(echo "obase=2;$ipPart3" | bc)
+ipPart1Binary=$(echo "${D2B[$ipPart1]}")
+ipPart2Binary=$(echo "${D2B[$ipPart2]}")
+ipPart3Binary=$(echo "${D2B[$ipPart3]}")
 
+if [ "${prefix}" -eq 24 ];
+then
+   networkIP=$(echo "$ipPart1Binary""$ipPart2Binary""$ipPart1Binary")
+   for i in {1..254}
+   do
+     hostIP=$(echo "${D2B[$i]}")
+     tempIPAddr=$(echo "$networkIP""$hostIP")
+     ipList+=$(echo "$IP Address ${i}: ${tempIPAddr} |")
+   done
+else
+   networkIP=$(echo "$ipPart1Binary""$ipPart2Binary")
+   numIPAddrs=0
+   for i in {1..254}
+   do
+     hostIP1=$(echo "${D2B[$i]}" )
+     for j in {1..254}
+     do
+       #numIPAddrs=$(echo $i + $j")
+       hostIP2=$(echo "${D2B[$j]}" )
+       tempIPAddr=$(echo "$networkIP""$hostIP1""$hostIP2")
+       ipList+="$IP Address : ${tempIPAddr} |"
+     done
+   done
+fi
 
-
-networkIP=$(echo "$ip" | cut -b 1-"$prefix")
-if [ "${prefix
-hostBits=$(echo 32 - "$prefix")
-numDifIPAddrs=$(echo "$hostBits" * 32 - "$hostBits" / 4)
-for i in {1.."${numDifIPAddrs}"}
-  do
-     hostIP=$(echo "obase=2;$i" | bc)
-     tempIPAddr=$(echo $"networkIP" + $"hostIP")
-     ipList+="$IP Address ${i}: ${tempIPAddr}"
-  done
-
-echo $"ipList" > ipCounter
+echo "${ipList}" > ipCounter.txt
