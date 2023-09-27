d053288a58128f43e98f2006f8e88805783d7e66
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-09-27 12:14:55 -0400

    uppdating active host

--- Week4/pingPossibleIPS.bash
+++ Week4/pingPossibleIPS.bash
@@ -6,9 +6,4 @@ while read -r line
 do
   checkIP=$(ping "${line}" -c 1)
   echo "${checkIP}" | grep "PING" | cut -d " " -f2 >> activehost.txt
-  #checkIP=$(ping -c 1 $line")
-  #if [ $checkIP" > /dev/null ];
-  #then
-  #   printf ${line}\n" >> activehosts.txt
-  #fi
 done < "$input"
