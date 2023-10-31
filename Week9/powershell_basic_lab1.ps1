d59b933afe3de8f7132af6b4a7809bffdd992219
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-10-31 19:23:13 -0400

    Submitting Power Sheel Basic Lab 1

---
+++ Week9/powershell_basic_lab1.ps1
@@ -0,0 +1,42 @@
+﻿(Get-NetIPAddress -AddressFamily IPv4 | where {$_.InterfaceAlias -ilike "Ethernet*"}).IPAddress
+(Get-NetIPAddress -AddressFamily IPv4 | where {$_.InterfaceAlias -ilike "Ethernet*"}).PrefixLength
+
+Get-WmiObject -List | where {$_.Name -ilike "Win32_[n][e][t]*"} | Sort-Object
+
+Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | select DHCPServer | Format-Table -HideTableHeaders
+
+(Get-DnsClientServerAddress -AddressFamily IPv4 | `
+where {$_.InterfaceAlias -ilike "Ethernet*"}).ServerAddresses[0]
+
+
+cd "C:\Users\champuser\CSI230-01JHannan\Week9"
+
+$files=(dir -Recurse)
+for($j=0; $j -le $files.Length; $j++){
+   if($files[$j].Extension -ilike "*ps1"){
+       $files[$j].Name
+    }
+}
+
+$folderPath = "C:\Users\champuser\CSI230-01JHannan\Week9\outfolder"
+if (Test-Path -Path $folderPath){
+  Write-Host "Folder Already Exists"
+}
+else{
+  New-Item -Name "outfolder" -ItemType Directory -Path $folderPath
+}
+
+cd "C:\Users\champuser\CSI230-01JHannan\Week9"
+$folderPath = "C:\Users\champuser\CSI230-01JHannan\Week9\outfolder\"
+$files=(Get-ChildItem)
+$filePath = Join-Path $folderPath "out.csv"
+$files | Where-Object {$_.Extension -like ".ps1"} | Export-Csv -Path $filePath
+
+$files=(Get-ChildItem -Recurse -File)
+#$files | Rename-Item -NewName {$_.Name -replace '.csv', '.log'}
+Get-ChildItem -Recurse -File
+
+
+
+
+
