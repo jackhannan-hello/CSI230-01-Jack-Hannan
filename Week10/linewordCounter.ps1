5909c3433fdcd3ba15e6e813f46f2881d5937e4c
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-11-06 15:43:11 -0500

    updating work on line/word count assigment

---
+++ Week10/linewordCounter.ps1
@@ -0,0 +1,5 @@
+﻿$FilesToLookAt = Get-ChildItem -Recurse -Filter "*.bash"
+
+for ($i=0; $i -lt $FilesToLookAt.Length; $i++){
+Get-Content $FilesToLookAt -TotalCount #| Measure-Object Lines, Words
+}
