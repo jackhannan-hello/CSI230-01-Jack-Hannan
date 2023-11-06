7d5e58cc7f1d07c250b637b9ba6eeb0d5efa2378
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-11-06 15:10:29 -0500

    Completing Get Service Logs assigment

---
+++ Week10/GetServiceLogs.ps1
@@ -0,0 +1,26 @@
+﻿$accteptedValues = @('all', 'stopped', 'running', 'quit')
+
+Write-Host "What services do you want to view: All Of Them, Running Services, or Stoped Services"
+$userView = Read-Host "Enter either: all, running, or stopped"
+
+while($userView -notin $accteptedValues){
+    $userView = Read-Host "Enter either: all, running, or stopped"
+}
+
+while($userView -notlike "quit"){
+    if($userView -ilike "all"){
+        Get-Service
+    }
+    elseif($userView -ilike "running"){
+        Get-Service | where {$_.Status -ilike "Running"}
+    }
+    else{
+        Get-Service | where {$_.Status -ilike "Stopped"}
+    }
+
+    $userView = Read-Host "Enter either: all, running, stopped, or quit"
+
+    while($userView -notin $accteptedValues){
+        $userView = Read-Host "Enter either: all, running, stopped, or quit"
+    }
+}
