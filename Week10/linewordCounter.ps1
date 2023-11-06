7edbef2ef8af5c044d194c2a7aa9e620ee186c9e
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-11-06 16:21:23 -0500

    completing World/Line Count assigment

--- Week10/linewordCounter.ps1
+++ Week10/linewordCounter.ps1
@@ -1,5 +1,13 @@
 ﻿$FilesToLookAt = Get-ChildItem -Recurse -Filter "*.bash"
 
+$filesInfo = @()
+
 for ($i=0; $i -lt $FilesToLookAt.Length; $i++){
-Get-Content $FilesToLookAt -TotalCount #| Measure-Object Lines, Words
+#Get-Content $FilesToLookAt[$i].FullName | Measure-Object -Line -Word
+$filesInfo += Get-Content $FilesToLookAt[$i].FullName | Measure-Object -Line -Word
 }
+
+Write-Host "Info for WORDS: "
+$filesInfo.Words | measure -Maximum -Minimum -Average
+Write-Host "Info for LINES: "
+$filesInfo.Lines | measure -Maximum -Minimum -Average
