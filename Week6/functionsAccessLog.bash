a4489b4a8c62b988d3458a29e045a6f59e82024b
Jack Hannan <jack.hannan@mymail.champlain.edu>
2023-11-08 12:59:47 -0500

    completing Report on Access Log assigment

--- Week6/functionsAccessLog.bash
+++ Week6/functionsAccessLog.bash
@@ -1,27 +1,37 @@
 input="logFile.txt"
+: > web1.html
 
-echo > web1.html
+echo "<html>" >> web1.html
+echo "<body>" >> web1.html
 
-echo \<table\> >> web1.html
-echo \<tr\> >> web1.html
-echo  \<th\>IP\<\/th\>" >> web1.html
-echo  \<th\>Time\<\/th\> >> web1.html
-echo  \<th\>RequestedPage\<\/th\> >> web1.html
-echo  \<th\>UserAgent\<\/th\> >> web1.html
-echo \<\/tr\>" >> web1.html
+echo "<table>" >> web1.html
+echo "<tr>" >> web1.html
+echo  "<th> IP </th>" >> web1.html
+echo  "<th> Time </th>" >> web1.html
+echo  "<th> RequestedPage </th>" >> web1.html
+echo  "<th> UserAgent </th>" >> web1.html
+echo "</tr>" >> web1.html
+
 
 while read -r line
 do
-echo "$line"
-echo $("$line" | cut -d " " -f1)
- echo \<tr\> >> web1.html
-  echo \<td\>$("${line}" | cut -d " " -f1)\<\/td\> >> web1.html
-  echo \<td\>$("${line}" | cut -d "[" -f2 | cut -d " " -f1)\<\/td\> >> web1.html
-  echo \<td\>$("${line}" | cut -d " " -f6 | cut -d "/" -f2)\<\/td\> >> web1.html
-  echo \<td\>$("${line}" | cut -d " " -f10)\<\/td\> >> web1.html
- echo \<\/tr\> >> web1.html
+#echo "$line"
+#echo "$line" | cut -d " " -f1
+echo "<tr>" >> web1.html
+IP=$(echo "${line}" | cut -d " " -f1)
+echo "<td>  ${IP}  </td>" >> web1.html
+TIME=$(echo "${line}" | cut -d "[" -f2 | cut -d " " -f1)
+echo "<td>  ${TIME}  </td>" >> web1.html
+requestedPage=$(echo "${line}" | cut -d " " -f6 | cut -d "/" -f2)
+echo "<td>  ${requestedPage}  </td>" >> web1.html
+userAgent=$(echo "${line}" | cut -d " " -f10)
+echo "<td>  ${userAgent}  </td>" >> web1.html
+echo "</tr>" >> web1.html
 done < "logFile.txt"
 
-echo \<\/table\> >> web1.html
+echo "</table>" >> web1.html
+
+echo "</body>" >> web1.html
+echo "</html>" >> web1.html
 
 cp web1.html /var/www/html/web1.html
